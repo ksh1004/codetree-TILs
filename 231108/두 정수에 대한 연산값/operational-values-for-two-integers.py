9587d97667cd01ab709a4cd03a54205086169f78
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-08 13:24:07 +0900

    Title: 두 정수에 대한 연산값, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 231108/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 231108/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -3,9 +3,14 @@ a, b = map(int, input().split())
 def func(num1, num2):
     max_val = max(num1, num2)
     min_val = min(num1, num2)
-    max_val += 25
-    min_val *= 2
-    return min_val, max_val
+    if(num1 == max_val):
+        num1 += 25
+        num2 *= 2
+        return num1, num2
+    else:
+        num1 *= 2
+        num2 += 25
+        return num1, num2
 
 n1, n2 = func(a, b)
 print(n1, n2)
