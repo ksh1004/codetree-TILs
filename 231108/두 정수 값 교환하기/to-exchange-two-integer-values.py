ca6f3efa07cf3cb385c03894909e86e882a755d8
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-08 13:09:38 +0900

    Title: 두 정수 값 교환하기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 231108/두 정수 값 교환하기/to-exchange-two-integer-values.py
+++ 231108/두 정수 값 교환하기/to-exchange-two-integer-values.py
@@ -4,7 +4,7 @@ def swap(num1, num2):
     temp = num1
     num1 = num2
     num2 = temp
-    return [num1, num2]
-    
-num_val = swap(n, m)
-print(num_val[0], num_val[1])
+    return num1, num2
+
+n, m = swap(n, m)    
+print(n, m)
