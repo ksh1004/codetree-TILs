df8ad748a95836d732e3ad0b3f83c294bb24892d
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-09 15:55:15 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 217ms, Memory: 26MB, Status: Passed - CodeTree

--- 231109/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 231109/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -8,24 +8,24 @@ def func(input_s, purpose):
         if(seq == 0 and input_s[i] == purpose[seq]): # 첫 문자열이 같은 경우
             idx = i
             seq += 1 # seq 1 증가
-            if(seq == len(input_s)): # 만약 seq 값과 입력 문자열이 같다면(즉, 목적 문자열이 존재하면)
+            if(seq == len(purpose)): # 만약 seq 값과 입력 문자열이 같다면(즉, 목적 문자열이 존재하면)
                 break
-            if(i == len(input_s) and seq < len(input_s)): # 끝까지 비교했는데 목적 문자열이 존재하지 않으면
+            if(i == len(input_s) - 1 and seq < len(purpose)): # 끝까지 비교했는데 목적 문자열이 존재하지 않으면
                 idx = -1
         elif(input_s[i] == purpose[seq]): # 이후 문자열이 같은 경우
             seq += 1 # seq 1 증가
-            if(seq == len(input_s)): # 만약 seq 값과 입력 문자열이 같다면(즉, 목적 문자열이 존재하면)
+            if(seq == len(purpose)): # 만약 seq 값과 입력 문자열이 같다면(즉, 목적 문자열이 존재하면)
                 break
-            if(i == len(input_s) and seq < len(input_s)): # 끝까지 비교했는데 목적 문자열이 존재하지 않으면
+            if(i == len(input_s) - 1 and seq < len(purpose)): # 끝까지 비교했는데 목적 문자열이 존재하지 않으면
                 idx = -1
         else: # 문자열이 다른 경우
             seq = 0
             if(input_s[i] == purpose[seq]): # 첫 목적 문자가 해당 문자열과 같은 경우
                 idx = i # 인덱스 재설정
                 seq += 1
-            if(seq == len(input_s)): # 만약 인덱스 값과 입력 문자열이 같다면(즉, 목적 문자열 길이가 1이고 존재하면)
+            if(seq == len(purpose)): # 만약 인덱스 값과 입력 문자열이 같다면(즉, 목적 문자열 길이가 1이고 존재하면)
                 break
-            if(i == len(input_s) and seq < len(input_s)): # 끝까지 비교했는데 목적 문자열이 존재하지 않으면
+            if(i == len(input_s) - 1 and seq < len(purpose)): # 끝까지 비교했는데 목적 문자열이 존재하지 않으면
                 idx = -1
     return idx
 
