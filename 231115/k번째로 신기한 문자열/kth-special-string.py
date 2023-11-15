e063436e2cbc2f7bbf209a62d7aec28723c8cdb8
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-15 11:32:45 +0900

    Title: k번째로 신기한 문자열, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231115/k번째로 신기한 문자열/kth-special-string.py
+++ 231115/k번째로 신기한 문자열/kth-special-string.py
@@ -5,9 +5,10 @@ str_list = []
 for i in range(n):
     s = input()
     check_val = 1
-    for j in range(len(T)):
-        if(s[j] != T[j]):
-            check_val = 0
+    if(len(s) <= len(T)):
+        for j in range(len(T)):
+            if(s[j] != T[j]):
+                check_val = 0
     if(check_val == 1):
         str_list.append(s)
 
