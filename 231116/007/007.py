05821677e7925c9b9e0466d8fbf2ee27bd197e35
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-16 19:18:52 +0900

    Title: 007, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/007/007.py
+++ 231116/007/007.py
@@ -1,12 +1,12 @@
 # 객체
-class cls:
+class Cls:
     def __init__(self, code, location, time):
         self.code = code
         self.location = location
         self.time = time
 
 code, location, time = input().split()
-cls1 = cls(code, location, time)
+cls1 = Cls(code, location, time)
 print(f'secret code : {cls1.code}')
 print(f'meeting point : {cls1.location}')
 print(f'time : {cls1.time}')
