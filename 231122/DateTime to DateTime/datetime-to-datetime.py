dccb1b060de3bcddf19ef5892fadb16d9907e7a3
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-22 14:45:57 +0900

    Title: DateTime to DateTime, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 231122/DateTime to DateTime/datetime-to-datetime.py
+++ 231122/DateTime to DateTime/datetime-to-datetime.py
@@ -1,10 +1,10 @@
 a, b, c = map(int, input().split())
 total = 0
-if(a == 11): # a가 11일이면
-    total += ((b - 1) - a) * 60 + (60 - 11) + (c)
-    print(total)
-elif((a < 11) or (a == 11 and b < 11) or (a == 11 and b == 11 and c < 11)):
+if((a < 11) or (a == 11 and b < 11) or (a == 11 and b == 11 and c < 11)): # 2011년 11월 11일 11시 11분보다 이전이면
     total = -1
+    print(total)
+elif(a == 11): # a가 11일이면
+    total += ((b - 1) - a) * 60 + (60 - 11) + (c)
     print(total)
 else: # a가 11일이 아니면
     total += (24 - 11) * 60 + (60 - 11)
