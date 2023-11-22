0a6d2a690d1190b5bb4d98216a1cedae59d8dbee
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-22 14:41:31 +0900

    Title: DateTime to DateTime, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231122/DateTime to DateTime/datetime-to-datetime.py
+++ 231122/DateTime to DateTime/datetime-to-datetime.py
@@ -3,6 +3,8 @@ total = 0
 if(a == 11): # a가 11일이면
     total += ((b - 1) - a) * 60 + (60 - 11) + (c)
     print(total)
+elif(a <= 11 and b <= 11 and c <= 11):
+    print(-1)
 else: # a가 11일이 아니면
     total += (24 - 11) * 60 + (60 - 11)
     total += ((a - 1) - 11) * 24 * 60
