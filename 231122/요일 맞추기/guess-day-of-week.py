5669fdd80479ea2c8c6fb4dc12cab1f0f456abd3
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-22 15:07:11 +0900

    Title: 요일 맞추기, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 231122/요일 맞추기/guess-day-of-week.py
+++ 231122/요일 맞추기/guess-day-of-week.py
@@ -1,26 +1,19 @@
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 m1, d1, m2, d2 = map(int, input().split())
-#12 23 12 24
-if((m1 < m2) or (m1 == m2 and d1 < d2)): # m1월 d1일이 m2월 d2일 보다 앞의 날짜인 경우
-    if(m1 == m2): # 월이 같은 경우
-        total = (d2 - d1) % 7
-        print(day[total])
-    else: # 월이 다른 경우
-        total = (num_of_days[m1] - d1)
-        for i in range((m1 + 1), m2):
-            total += num_of_days[i]
-        total += d2
-        total %= 7
-        print(day[total])
-else: # m1월 d1일이 m2월 d2일 보다 뒤의 날짜인 경우
-    if(m1 == m2): # 월이 같은 경우
-        total = (d2 - d1) % 7
-        print(day[total])
-    else: # 월이 다른 경우
-        total = (num_of_days[m2] - d2)
-        for i in range((m2 + 1), m1):
-            total += num_of_days[i]
-        total += d1
-        total %= 7
-        print(day[total])
+
+# 두 날짜의 총 일수 구하기
+total_day1, total_day2 = 0, 0
+for i in range(1, m1):
+    total_day1 += num_of_days[i]
+total_day1 += d1
+for i in range(1, m2):
+    total_day2 += num_of_days[i]
+total_day2 += d2
+
+# 두 날짜 간 차이 구하기
+diff = total_day2 - total_day1
+#while(diff < 0): # 음수인 경우에는 양수로 전환
+#    diff += 7
+diff %= 7
+print(day[diff])
