7b8d73311d039ab99e947f35808e32abe56eb7c2
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-22 15:01:52 +0900

    Title: 요일 맞추기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231122/요일 맞추기/guess-day-of-week.py
+++ 231122/요일 맞추기/guess-day-of-week.py
@@ -1,9 +1,10 @@
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 m1, d1, m2, d2 = map(int, input().split())
+#12 23 12 24
 if((m1 < m2) or (m1 == m2 and d1 < d2)): # m1월 d1일이 m2월 d2일 보다 앞의 날짜인 경우
     if(m1 == m2): # 월이 같은 경우
-        total = (d1 - d2) % 7
+        total = (d2 - d1) % 7
         print(day[total])
     else: # 월이 다른 경우
         total = (num_of_days[m1] - d1)
