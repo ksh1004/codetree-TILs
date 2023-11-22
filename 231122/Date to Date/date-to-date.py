08f183d81a47d043dee9ed3cd4f815fceacc780b
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-22 14:14:07 +0900

    Title: Date to Date, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 231122/Date to Date/date-to-date.py
+++ 231122/Date to Date/date-to-date.py
@@ -1,7 +1,7 @@
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 m1, d1, m2, d2 = map(int, input().split())
 if(m1 == m2): # 월이 같은 경우
-    print(d2 - d1)
+    print(d2 - d1 + 1)
 else: # 월이 다른 경우
     total = 0
     for i in range(m1 + 1, m2): # m1, m2 월 사이의 날들을 더함
