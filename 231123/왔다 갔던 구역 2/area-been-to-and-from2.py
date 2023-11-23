2ef5930c9898b68127e994913030ae3748a5294e
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-23 14:54:28 +0900

    Title: 왔다 갔던 구역 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231123/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 231123/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -6,15 +6,21 @@ for i in range(n):
     num, order = input().split()
     num = int(num)
     if(order == 'L'):
-        for j in range(idx, idx - num, -1):
+        for j in range(idx, idx - num - 1, -1):
             cnt[j] += 1
         idx = idx - num
     else:
-        for j in range(idx, idx + num):
+        for j in range(idx, idx + num + 1):
             cnt[j] += 1
         idx = idx + num
 total = 0
+idx = -1 # 인덱스 변수 재활용
 for i in range(len(cnt)):
-    if(cnt[i] >= 2):
-        total += 1
+    if(cnt[i] >= 2): # 만약 2번 이상 지나간 영역이면
+        if(idx == -1): # 근데 구간 설정이 안되어있으면
+            idx = i # 구간 설정
+    else: # 2번 미만으로 지나간 영역이면
+        if(idx != -1): # 이전에 2번 이상 지나간 구간이 있는 경우
+            total += ((i - 1) - idx) # 해당 구간을 더한다(i - 1로 하는 이유는, 현재 i값은 2번 이상 이어진 것이 아니고, 직전 i값까지 이어진 것이기 때문)
+            idx = -1
 print(total)
