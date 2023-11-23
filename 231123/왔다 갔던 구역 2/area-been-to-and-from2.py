aaf899897f6dbd0dfd0a27f9187f373860a2f35d
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-23 15:11:44 +0900

    Title: 왔다 갔던 구역 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231123/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 231123/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,16 +1,17 @@
 n = int(input())
 cnt = [0] * 201
-cnt[100] += 1 # -100 ~ 100에서 0 ~ 200으로 계산하고, 위치 0 -> 100에서 시작하므로 해당 위치에 1 증가
+#cnt[100] += 1 
 idx = 100 # 시작 인덱스
+# -100 ~ 100에서 0 ~ 200으로 계산하고, 위치 0 -> 100에서 시작하므로 해당 위치에 1 증가
 for i in range(n):
     num, order = input().split()
     num = int(num)
     if(order == 'L'):
-        for j in range(idx, idx - num, -1):
+        for j in range(idx, idx - num - 1, -1):
             cnt[j] += 1
         idx = idx - num
     else:
-        for j in range(idx, idx + num):
+        for j in range(idx, idx + num + 1):
             cnt[j] += 1
         idx = idx + num
 total = 0
@@ -21,6 +22,6 @@ for i in range(len(cnt)):
             idx = i # 구간 설정
     else: # 2번 미만으로 지나간 영역이면
         if(idx != -1): # 이전에 2번 이상 지나간 구간이 있는 경우
-            total += (i - idx) # 해당 구간을 더한다(i - 1로 하는 이유는, 현재 i값은 2번 이상 이어진 것이 아니고, 직전 i값까지 이어진 것이기 때문)
+            total += ((i - 1) - idx) # 해당 구간을 더한다(i - 1로 하는 이유는, 현재 i값은 2번 이상 이어진 것이 아니고, 직전 i값까지 이어진 것이기 때문)
             idx = -1
 print(total)
