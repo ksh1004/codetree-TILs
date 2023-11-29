67405b600cda7f99296ec5e4d249d3e405a5168d
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-11-29 16:05:00 +0900

    Title: 작은 구슬의 이동, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 231129/작은 구슬의 이동/small-marble-movement.py
+++ 231129/작은 구슬의 이동/small-marble-movement.py
@@ -1,24 +1,20 @@
-# 해결 실패
+# 해설 코드
 n, t = map(int, input().split())
 r, c, d = input().split()
 r, c = int(r), int(c)
 dx, dy = [0, 1, -1, 0], [1, 0, 0, -1] # 북, 동, 서, 남 순서(남, 북이 서로 반대고, 서, 동이 서로 반대임)
-direction = -1 # 방향 설정 변수
-# 방향 설정
-if(d == 'U'): # 위
-    direction = 1
-elif(d == 'D'): # 아래
-    direction = 2
-elif(d == 'R'): # 오른
-    direction = 0
-elif(d == 'L'): # 왼
-    direction = 3
-# 이동 계산
-for i in range(t):
+mapper = {
+    'R': 0,
+    'D': 1,
+    'U': 2,
+    'L': 3
+}
+direction = mapper[d]
+for _ in range(t):
     nx, ny = r + dx[direction], c + dy[direction]
-    if(nx >= 1 and nx <= n and ny >= 1 and ny <= n): # 이동한 값이 격자 안이라면
+    if(1 <= nx and nx <= n and 1 <= ny and ny <= n):
         r, c = nx, ny
-    else: # 이동한 값이 격자 밖이라면
-        direction = 3 - direction # 이동방향 반대방향으로 변경
+    else:
+        direction = 3 - direction
 # 출력
 print(r, c)
