7cd37d556d0f391cc7616f14e0c816592c4b8754
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2023-12-07 15:31:36 +0900

    Title: 괄호 쌍 만들어주기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231207/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,11 @@
+A = input()
+cnt = 0
+for i in range(len(A)):
+    if(A[i] == '('):
+       if(A[i + 1] == '('):
+        for j in range(i + 2, len(A)):
+            if(j != len(A) - 1 and A[j] == ')'):
+                if(A[j + 1] == ')'):
+                    cnt += 1
+# 출력
+print(cnt)
