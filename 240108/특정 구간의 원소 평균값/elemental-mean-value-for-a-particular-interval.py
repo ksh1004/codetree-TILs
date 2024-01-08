5850351dc96b86dd7c18eb230dc28d41cdb201bc
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2024-01-08 11:47:20 +0900

    Title: 특정 구간의 원소 평균값, Time: 129ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240108/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240108/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -9,9 +9,11 @@ for i in range(N): # 구간의 시작 인덱스
             temp = num_list[k] # 구간 값 추가
             temp_cnt += 1 # 크기 1 증가
         temp_avg = temp / temp_cnt # 구간의 평균값
+        check = False
         for k in range(i, j + 1):
             if(temp_avg == num_list[k]): # 구간 평균값이 구간 내 원소값과 동일하면
-                cnt += 1 # 횟수 1 증가
-                break
+                check = True
+        if(check == True):
+            cnt += 1
 # 출력
 print(cnt)
