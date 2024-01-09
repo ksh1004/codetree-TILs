9a96763ccbf9d7ceeb574a39677bf152c1f271a6
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2024-01-09 11:04:07 +0900

    Title: 밭의 높이를 고르게하기, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240109/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 240109/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -1,11 +1,20 @@
-N, H, T = map(int, input().split())
-num_list = list(map(int, input().split()))
-min_val = 999999999
-# 최소 비용 계산
-for i in range(N - T + 1):
-    temp = 0
-    for j in range(i, i + T):
-        temp += abs(j - H)
-    min_val = min(min_val, temp)
-# 출력
-print(min_val)
+import sys
+
+INT_MAX = sys.maxsize
+
+# 변수 선언 및 입력:
+n, h, t = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+   
+# 모든 구간의 시작점을 잡아봅니다.
+min_cost = INT_MAX
+for i in range(n - t + 1):
+    # 해당 구간을 고르게 할 때의 비용을 구합니다.
+    cost = 0
+    for j in range(i, i + t):
+        cost += abs(arr[j] - h)
+    
+    # 최솟값을 구합니다.
+    min_cost = min(min_cost, cost)
+
+print(min_cost)
