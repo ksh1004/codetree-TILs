0ef3ebdcaa49da76631cdb252fe81b16f0a7344a
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2025-04-04 21:27:55 +0900

    Title: 스승의 은혜 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250404/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,19 @@
+N, B = map(int, input().split())
+P = [int(input()) for _ in range(N)]
+max_student = 0
+
+for i in range(N):
+    price = 0
+    num_student = 0
+    for j in range(N):
+        if(i == j):
+            price += P[j] / 2
+        else:
+            price += P[j]
+        if(price > B):
+            break
+        else:
+            num_student += 1
+    max_student = max(max_student, num_student)
+
+print(max_student)
