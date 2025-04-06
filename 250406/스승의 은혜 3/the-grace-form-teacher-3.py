bfda0bdb71fc53864c50c5806a9ef64a268992ea
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2025-04-06 20:27:36 +0900

    Title: 스승의 은혜 3, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250406/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250406/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -6,15 +6,25 @@ max_cnt = 0
 for i in range(N):
     price = 0
     cnt = 0
+    price += gifts[i][0] // 2 + gifts[i][1] # 할인 쿠폰 사용 가격
+
+    if(price > B): # 할인 쿠폰 적용해도 예산 초과시
+        continue # 패스
+    cnt += 1
+    other = [] # 나머지 선물 값들을 저장할 리스트
     for j in range(N):
-        if(i == j): # 반값 쿠폰 적용대상
-            price += gifts[j][0] // 2 + gifts[j][1]
-        else:
-            price += gifts[j][0] + gifts[j][1]
-        if(price <= B):
-            cnt += 1
-        else:
-            break
-    max_cnt = max(max_cnt, cnt)
+        if(i == j):
+            continue
+        other.append(gifts[j][0] + gifts[j][1])
     
+    other.sort() # 선물 값들을 정렬
+    for i in other:
+        price += i
+        if(price <= B): # 예산 초과가 아닌 경우
+            cnt += 1 # 추가
+        else: # 예산 초과시
+            break # 종료
+    max_cnt = max(max_cnt, cnt)
+
+# 출력
 print(max_cnt)
