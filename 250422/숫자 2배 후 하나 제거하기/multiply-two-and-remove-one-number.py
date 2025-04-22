78263f44ad6b7c2e35930c8faf4989aa56fe2f51
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2025-04-22 21:00:29 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 75ms, Memory: 17MB, Status: Passed - Codetree

--- 250422/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
+++ 250422/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
@@ -1,19 +1,16 @@
 N = int(input())
 num_list = list(map(int, input().split()))
-min_total = float('inf')
+min_total = float('inf') # 최솟값
 
 for i in range(N):
-    # i번째 값을 2배로 만든 새로운 리스트
     doubled_list = num_list[:]
     doubled_list[i] *= 2
-
     for j in range(N):
-        temp_list = doubled_list[:j] + doubled_list[j+1:]
-
         now_total = 0
+        temp_list = doubled_list[:j] + doubled_list[j+1:] # 하나의 숫자를 제거
         for k in range(1, len(temp_list)):
-            now_total += abs(temp_list[k] - temp_list[k - 1])
+            val = abs(temp_list[k] - temp_list[k - 1])
+            now_total += val
+        min_total = min(min_total, now_total) # 최솟값 업데이트
 
-        min_total = min(min_total, now_total)
-
-print(min_total)
+print(min_total) # 출력
