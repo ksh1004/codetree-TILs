649b5f938bc4977b539887e2b0518c9da0636f33
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2025-07-20 19:18:53 +0900

    Title: 독서실의 거리두기 5, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250720/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -0,0 +1,25 @@
+n = int(input())
+seat = list(input())
+
+def min_dist():
+    dist = n
+    # 둘 다 1인 곳에 대해 모든 쌍을 조사하여, 그 중 가장 가까운 거리를 구하기
+    for i in range(n):
+        for j in range(i + 1, n):
+            if seat[i] == '1' and seat[j] == '1':
+                dist = min(dist, j - i)
+    
+    return dist
+
+ans = 0
+# 들어갈 위치를 일일이 정해보며 그 상황에서 가장 가까운 사람간의 거리를 구해 가능한 경우 중 최댓값을 계산
+for i in range(n):
+    if seat[i] == '0': # 비어있는 위치에 인원을 배치
+        seat[i] = '1' # 가장 가까운 사람간의 거리를 구해 최댓값을 갱신
+        ans = max(ans, min_dist()) # 다시 채워졌던 값을 되돌려주기
+        seat[i] = '0'
+
+print(ans)
+ 
+
+
