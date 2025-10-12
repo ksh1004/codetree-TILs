40704f999719abdf6b0bd89341434098903e403c
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2025-10-12 16:09:49 +0900

    Title: 전부 겹치는 선분 2, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251012/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -0,0 +1,24 @@
+N = int(input())
+grid = [list(map(int, input().split())) for _ in range(N)]
+
+answer = False
+
+for i in range(N):
+    sep = [0 for _ in range(101)]
+    for j in range(N):
+        if(i == j):
+            continue
+        else:
+            for k in range(grid[i][0], grid[i][1] + 1):
+                sep[k] += 1
+    for j in range(101):
+        if(sep[j] == N - 1):
+            answer = True
+            break
+    if(answer == True):
+        break
+
+if(answer):
+    print('Yes')
+else:
+    print('No')        
