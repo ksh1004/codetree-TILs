925744ebeecf4d847a356206d391958f473d5f44
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2025-10-12 15:59:23 +0900

    Title: 구역 청소, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251012/구역 청소/district-cleaning.py
+++ 251012/구역 청소/district-cleaning.py
@@ -1,7 +1,15 @@
 a, b = map(int, input().split())
 c, d = map(int, input().split())
+sep = [0 for _ in range(101)]
 
-min_val = min(a, c)
-max_val = max(b, d)
+for i in range(a, b):
+    sep[i] = 1
+for j in range(c, d):
+    sep[j] = 1
 
-print(max_val - min_val)
+cnt = 0
+for k in range(0, 101):
+    if(sep[k] == 1):
+        cnt += 1
+
+print(cnt)
