7e5d1755a5ecac4c512698f2ad94be08f7e4cfa3
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2026-09-16 13:33:28 +0900

    [Passed] 마을 구분하기 (52ms, 16MB)

--- trail4/마을 구분하기/seperate-village.py
+++ trail4/마을 구분하기/seperate-village.py
@@ -4,13 +4,11 @@ grid = [list(map(int, input().split())) for _ in range(N)]
 visited = [[False for _ in range(N)] for _ in range(N)]
 
 people_num = 0
-people_nums = list()
+people_nums = []
 
-# 주어진 위치가 격자 내부에 있는지 확인
 def in_range(x, y):
     return 0 <= x < N and 0 <= y < N
 
-# 주어진 위치로 이동할 수 있는지 여부
 def can_go(x, y):
     if not in_range(x, y):
         return False
@@ -23,30 +21,28 @@ def can_go(x, y):
 def dfs(x, y):
     global people_num
 
-    # 우 하 좌 상
-    dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+    # 상 하 좌 우
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
 
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
-    
-        if can_go(nx, ny):
+
+        if(can_go(nx, ny)):
             visited[nx][ny] = True
             people_num += 1
             dfs(nx, ny)
-
+    
 for i in range(N):
     for j in range(N):
-        if can_go(i, j):
-            visited[i][j] = True
+        if(can_go(i, j)):
             people_num = 1
-
+            visited[i][j] = True
             dfs(i, j)
-
             people_nums.append(people_num)
 
 people_nums.sort()
 
 print(len(people_nums))
-
-for i in range(len(people_nums)):
-    print(people_nums[i])
+for i in people_nums:
+    print(i)
