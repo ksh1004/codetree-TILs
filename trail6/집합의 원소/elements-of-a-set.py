38da13208605622ab974c208b29a0d118fca6d64
ksh1004 <67865103+ksh1004@users.noreply.github.com>
2026-09-17 11:29:57 +0900

    [Passed] 집합의 원소 (366ms, 25MB)

--- trail6/집합의 원소/elements-of-a-set.py
+++ trail6/집합의 원소/elements-of-a-set.py
@@ -1,33 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-def find(x):
-    while parent[x] != x:
-        parent[x] = parent[parent[x]]  # 경로 압축 (경로 절반 압축)
-        x = parent[x]
-    return x
+N, M = map(int, input().split())
+
+arr = [i for i in range(N + 1)]
+
+# find 함수
+def find(a):
+    if(a != arr[a]):
+        arr[a] = find(arr[a])
+    return arr[a]
 
 def union(a, b):
-    ra, rb = find(a), find(b)
-    if ra == rb:
-        return
-    # union by rank
-    if rank[ra] < rank[rb]:
-        ra, rb = rb, ra
-    parent[rb] = ra
-    if rank[ra] == rank[rb]:
-        rank[ra] += 1
+    r1 = find(a)
+    r2 = find(b)
+    arr[r1] = r2
 
-n, m = map(int, input().split())
-parent = list(range(n + 1))
-rank = [0] * (n + 1)
-
-result = []
-for _ in range(m):
-    op, a, b = map(int, input().split())
-    if op == 0:
+for i in range(M):
+    num, a, b = map(int, input().split())
+    if num == 0:
         union(a, b)
     else:
-        result.append('1' if find(a) == find(b) else '0')
-
-print('\n'.join(result))
+        if(find(a) == find(b)):
+            print(1)
+        else:
+            print(0)
